8f76912afc64338473cd67985c72174536563cce
SaulRecio06 <76199088+SaulRecio06@users.noreply.github.com>
2022-09-19 00:13:39 -0600

    Primer Subida

---
+++ Juego terror uas/InputConstants.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public class InputConstants
+{
+	public const string walk_right = "walk_right";
+	
+		public const string walk_left = "walk_left";
+			public const string walk_up = "walk_up";
+				public const string walk_down = "walk_down";
+}
